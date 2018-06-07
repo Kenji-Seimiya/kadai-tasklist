328142f24416f30def00663c55113890bcf38ad3
koiking <koiking330@yahoo.co.jp>
2018-06-07 02:23:30 +0000

    top page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @tasks = @user.tasks
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザーを登録しました。'
+    redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザー登録に失敗しました。'
+      render 'tasks/index'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
