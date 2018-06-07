7f550607789f733f7b4ab868c4593851465cbd12
koiking <koiking330@yahoo.co.jp>
2018-06-07 07:44:50 +0000

    user login

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
-    validates :status, presence: true, length: {maximum: 10} 
-    validates :content, presence: true, length: {maximum: 255}
-    
     belongs_to :user 
     
+    validates :status, presence: true, length: {maximum: 10} 
+    validates :content, presence: true, length: {maximum: 255}
+    validates :user_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
 
-  get 'users/index'
+  get 'sessions/new'
 
-  get 'users/show'
+  get 'sessions/create'
 
-  get 'users/new'
+  get 'sessions/destroy'
 
-  get 'users/create'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  root to: 'toppages#index'
+  
   resources :tasks
   
   get 'signup', to: 'users#new'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  
+  
+  
   resources :users, only: [:index, :show, :new, :create]
   
 end
